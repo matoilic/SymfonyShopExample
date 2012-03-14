2a643d359c7010c1349a9c234fab53eb205bba65
Mato Ilic <info@matoilic.ch>
2012-03-14 23:51:01 +0100

    method to retrieve the current user

--- src/Shop/BackendBundle/Controller/Controller.php
+++ src/Shop/BackendBundle/Controller/Controller.php
@@ -6,5 +6,11 @@ use Shop\CommonBundle\Controller\Controller as BaseController;
 
 abstract class Controller extends BaseController
 {
-
+    /**
+     * @return \Shop\CommonBundle\Entity\User
+     */
+    protected function getCurrentUser()
+    {
+        return $this->get('security.context')->getToken()->getUser();
+    }
 }
